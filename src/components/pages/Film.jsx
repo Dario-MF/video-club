425b40b873b0a92af83fa701ae5ef5b1802c19ce
Dario <ferrada86@gmail.com>
2021-07-24 13:41:21 +0200

    update

--- src/components/pages/Film.jsx
+++ src/components/pages/Film.jsx
@@ -8,7 +8,6 @@ import CarouselForRecomendations from '../organisms/CarouselForRecomendations'
 
 
 const Film = ({match})=>{
-    console.log(match.params)
     const film = useFetch(`https://api.themoviedb.org/3/movie/${match.params.id}?api_key=${Api.apiKey}&language=es&include_video=true`, [])
     const video = useFetch(`https://api.themoviedb.org/3/movie/${match.params.id}/videos?api_key=${Api.apiKey}&language=en-US`, [])
     const recomendations = useFetch(`https://api.themoviedb.org/3/movie/${match.params.id}/recommendations?api_key=${Api.apiKey}&language=es&page=1`, [])
